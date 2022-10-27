c92b25e75f0c99c5d38e7fe9a0ee3444c8a40e39
Prabir Singh <codeprabir@gmail.com>
2022-10-27 18:25:44 +0530

    array method in javascript

---
+++ app.js
@@ -0,0 +1,22 @@
+const numbers = ['one', 'two', 'three', 'four']
+const colors = ['red', 'green', 'blue']
+
+// // Length method
+// console.log(numbers.length)
+
+// push method
+// numbers.push("five");
+// console.log(numbers)
+
+//pop method removes the last item of the array
+// const lastnum = numbers.pop();
+// console.log(numbers)
+// console.log(lastnum)
+// also returns the deleted item
+
+//indexOf method provides you the index of the array;
+// console.log(numbers.indexOf('three'))
+
+// shift() method removes the first item from array;
+// colors.shift()
+// console.log(colors)
